fe88707d5c80c1633cd4efadbcc9b3d0e63f749f
KrystianGrabowski <krystian987543@gmail.com>
2017-11-29 18:30:33 +0100

    first commit

---
+++ osadnicy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <pthread.h>
+#include <stdlib.h>
+
+int mysliwi, kucharze, zwierzyna, pozywienie;
+
+
+int main(int argc, char* argv[]){
+
+    mysliwi = atoi(argv[1]);
+    kucharze = atoi(argv[2]);
+    zwierzyna = atoi(argv[3]);
+    pozywienie = atoi(argv[4]);
+    
+}
