a068479cafd1f4e1742bb314de22987cddce10a8
KrystianGrabowski <krystian987543@gmail.com>
2017-11-29 20:05:13 +0100

    synch

--- osadnicy.cpp
+++ osadnicy.cpp
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <unistd.h>
+#include <stdio.h>
 
 int mysliwi, kucharze, zwierzyna, pozywienie;
+int ocalali;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 
 int kostka_rzut(){
     return (std::rand() % 6) + 1;
@@ -14,32 +19,44 @@ void polowanie(){
 
     int mysliwy_rzut = kostka_rzut();
     int zwierz_rzut = kostka_rzut();
-
+    pthread_mutex_lock(&lock);
     if (mysliwy_rzut > zwierz_rzut) {
         zwierzyna++;
-
+        printf("Udane polowanie +1 <zwierzyna {%d}>\n", zwierzyna);
     }
+    pthread_mutex_unlock(&lock);
+
+
 }
 
 void pieczenie(){
-
+    pthread_mutex_lock(&lock);
     if (zwierzyna > 0){
         zwierzyna--;
         int kucharz_rzut = kostka_rzut();
         pozywienie += kucharz_rzut;
+        printf("Udane pieczenie +%d <pozywienie {%d}>\n", kucharz_rzut, pozywienie);
     }
+    pthread_mutex_unlock(&lock);
 }
 
 void *mysliwy(void*){
     for (int dzien = 1; dzien <= 365; dzien++){
-        polowanie();
 
+        polowanie();
+        pthread_mutex_lock(&lock);
         if (pozywienie > 0){
             pozywienie--;
+            pthread_mutex_unlock(&lock);
             sleep(1);
         }
-        else
+        else{
+            printf("Mysliwy odszedl z wioski !!\n");
+            ocalali--;
+            pthread_mutex_unlock(&lock);
             break;
+
+        }
 
     }
     return 0;
@@ -47,14 +64,21 @@ void *mysliwy(void*){
 
 void *kucharz(void*){
     for (int dzien = 1; dzien <= 365; dzien++){
-        pieczenie();
 
+        pieczenie();
+        pthread_mutex_lock(&lock);
         if (pozywienie > 0 ){
             pozywienie--;
+            pthread_mutex_unlock(&lock);
             sleep(1);
         }
-        else
+        else{
+            printf("Kucharz odszedl z wioski !!\n");
+            ocalali--;
+            pthread_mutex_unlock(&lock);
             break;
+        }
+
     }
     return 0;
 }
@@ -62,12 +86,13 @@ void *kucharz(void*){
 int main(int argc, char* argv[]){
 
     int k, m;
-    
+
     srand( time( NULL ) );
     mysliwi = atoi(argv[1]);
     kucharze = atoi(argv[2]);
     zwierzyna = atoi(argv[3]);
     pozywienie = atoi(argv[4]);
+    ocalali = mysliwi + kucharze;
 
     pthread_t tab_k[kucharze];
     pthread_t tab_m[mysliwi];
@@ -85,9 +110,8 @@ int main(int argc, char* argv[]){
     for (m = 0; m < kucharze; m++){
         pthread_join(tab_m[m], NULL);
     }
-    std::cout << pozywienie;
+    pthread_mutex_destroy(&lock);
+    printf("\n\n Podsumowanie :\n ocalali = %d \n pozywnie = %d\n", ocalali, pozywienie);
 
-
-
-
+    return 0;
 }
